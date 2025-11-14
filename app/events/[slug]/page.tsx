2d003f268792c3d825416ee188e20ff6d28f6b5d
arifframd <arifframadh08@gmail.com>
2025-11-14 14:08:45 +0700

    menambahkan suspense

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,112 +1,15 @@
-import BookEvent from "@/components/BookEvent";
-import EventCard from "@/components/EventCard";
-import { IEvent } from "@/database/event.model";
-import { getSimilarEventsBySlug } from "@/lib/action/event.action";
-import { cacheLife } from "next/cache";
-import Image from "next/image";
-import { notFound } from "next/navigation";
-import React from "react";
-
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-
-const EventDetailItem = ({ icon, alt, label }: { icon: string; alt: string; label: string }) => {
-  return (
-    <div className="flex-row-gap-2 items-center">
-      <Image src={icon} alt={alt} width={18} height={18} />
-      <p>{label}</p>
-    </div>
-  );
-};
-
-const EventAgenda = ({ agenda }: { agenda: string[] }) => {
-  return (
-    <div className="agenda">
-      <h2>Agenda</h2>
-      <ul>
-        {agenda.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-const EventTags = ({ tags }: { tags: string[] }) => {
-  return (
-    <div className="flex flex-row gap-1.5 flex-wrap">
-      {tags.map((tag, index) => (
-        <div className="pill" key={index}>
-          {tag}
-        </div>
-      ))}
-    </div>
-  );
-};
+import EventDetail from "@/components/EventDetail";
+import { Suspense } from "react";
 
 const EventDetailPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
-  "use cache";
-  cacheLife("hours");
-  const { slug } = await params;
-  const res = await fetch(`${BASE_URL}/api/event/${slug}`);
-  const {
-    event: { _id, description, overview, image, location, date, time, mode, audience, agenda, organizer, tags },
-  } = await res.json();
+  const slug = params.then((p) => p.slug);
 
-  const booking = 10;
-  if (!description) return notFound();
-
-  const similarEvents: IEvent[] = await getSimilarEventsBySlug(slug);
   return (
-    <section id="event">
-      <div className="header">
-        <h1>Event Description</h1>
-        <p className="mt-2">{description}</p>
-      </div>
-
-      <div className="details">
-        {/* Left side - Event Content */}
-        <div className="content">
-          <Image src={image} alt="Event image" width={800} height={800} className="banner" />
-
-          <section className="flex-col-gap-2">
-            <h2>Overview</h2>
-            <p>{overview}</p>
-          </section>
-
-          <section className="flex-col-gap-2">
-            <h2>Event Details</h2>
-            <EventDetailItem icon="/icons/calendar.svg" alt="calendar" label={date} />
-            <EventDetailItem icon="/icons/clock.svg" alt="clock" label={time} />
-            <EventDetailItem icon="/icons/pin.svg" alt="location" label={location} />
-            <EventDetailItem icon="/icons/mode.svg" alt="mode" label={mode} />
-            <EventDetailItem icon="/icons/audience.svg" alt="audience" label={audience} />
-          </section>
-
-          <EventAgenda agenda={agenda} />
-
-          <section className="flex-col-gap-2">
-            <h2>About the Organizer</h2>
-            <p>{organizer}</p>
-          </section>
-
-          <EventTags tags={tags} />
-        </div>
-
-        {/* Right side - Booking Form */}
-        <aside className="booking">
-          <div className="signup-card">
-            <h2>Book Your Spot</h2>
-            {booking > 0 ? <p className="text-sm">Join {booking} who already booked their spot</p> : <p className="text-sm">Be the first to book your spot!</p>}
-            <BookEvent eventId={_id} slug={slug} />
-          </div>
-        </aside>
-      </div>
-
-      <div className="flex w-full flex-col gap-4 pt-20">
-        <h2>Similar Events</h2>
-        <div className="events">{similarEvents.length > 0 && similarEvents.map((similarEvent: IEvent) => <EventCard key={similarEvent.slug} {...similarEvent} />)}</div>
-      </div>
-    </section>
+    <main>
+      <Suspense fallback={<div>Loading event details...</div>}>
+        <EventDetail params={slug} />
+      </Suspense>
+    </main>
   );
 };
 
